9c20f02256f64c9ba44872b561f96372dce7726f
Akshat <jainakshat252003@gmail.com>
2026-08-05 17:48:50 +0530

    deploy-backend

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World bhadwaeeeeeeeee!';
+    return 'Hello World bhadwaiiiiiii!';
   }
 }
